ae27162ddb1b9cc1ab62c6212fbeebedac4b193d
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-06-18 23:11:57 +0200

    add controll using gamepad api

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import GameItem from "./components/GameItem.vue";
-import { exec } from 'child_process'
+import { exec } from "child_process";
 
 const games = [
   {
@@ -31,7 +31,7 @@ const games = [
     description:
       "Budova(nie) entity je hra, v ktorej sa dozvedáš o spomienkach a histórii budovy, ktorá to v minulosti nemala ľahké. Vytvor si vlastný názor a porozmýšľaj o jej osude.",
     authors: "Petra Kořenková, Kristian Shofranko, Dominik Devečka",
-    gamePath: "open /Applications/LittleMousesEncyclopedia.app"
+    gamePath: "open /Applications/LittleMousesEncyclopedia.app",
   },
   {
     name: "Paper factory",
@@ -39,7 +39,7 @@ const games = [
     description:
       "Izometrický environment je simuláciou balansu medzi industriálnou výrobou a pôvodnou prírodou. Abstrahovanie rastu a nepomeru do vizuálnej čistej hry. Industrializácia miest ponúka rozvoj, pre pochopenie exponencionálneho rastu je možné simulovať rôzne scenáre aktivity.",
     authors: "Leonard Lofaj, Prokop Findeis",
-    gamePath: "open /Applications/LittleMousesEncyclopedia.app"
+    gamePath: "open /Applications/LittleMousesEncyclopedia.app",
   },
   {
     name: "Luhy",
@@ -47,7 +47,7 @@ const games = [
     description:
       "Simulátor chodenia v prostredí Trenčianskych Luhov. V prostredí je možné prežiť problematiku približovania sa k chráneným živočíchom, premenu vzácnych biotopov na sklad odpadu.",
     authors: "Victoria Ann Bračoková, Ján Konečný, Miroslav Čuridlo",
-    gamePath: "open /Applications/LittleMousesEncyclopedia.app"
+    gamePath: "open /Applications/LittleMousesEncyclopedia.app",
   },
   {
     name: "440",
@@ -55,41 +55,101 @@ const games = [
     description:
       "V 3D interaktívnej hre sa môžeme prechádzať prostredím skládky chemického odpadu z bývalých Chemických závodov Juraja Dimitrova v bratislavskej mestskej časti Vrakuňa v období neskoršieho antropocénu, v ktorom už georeliéf nie je tvorený len prírodnými horninami, ale obsahuje stopy a produkty ľudských činností.",
     authors: "Adela Lujza Lučeničová, Natália Zajáčiková, Emma Záhradníková",
-    gamePath: "open /Applications/LittleMousesEncyclopedia.app"
+    gamePath: "open /Applications/LittleMousesEncyclopedia.app",
   },
 ];
 const focused = ref(0);
+let gamepadIndex = ref(null);
 
 onMounted(() => {
+  window.addEventListener("gamepadconnected", (event) => {
+    console.log("A gamepad connected:");
+    gamepadIndex.value = event.gamepad.index;
+  });
+  window.addEventListener("gamepaddisconnected", () => {
+    gamepadIndex.value = null;
+  });
   window.addEventListener("keypress", (e) => {
     console.log(e.keyCode);
     switch (e.keyCode) {
       case 97: // left
-        focused.value--;
+        left();
         break;
       case 100: //right
-        focused.value++;
+        right();
         break;
       case 119: // up
-        focused.value = focused.value - 2;
+        up();
         break;
       case 115: // down
-        focused.value = focused.value + 2;
+        down();
         break;
       case 13: // enter
         launchGame(focused.value);
         break;
     }
-    focused.value = mod(focused.value, 4);
   });
 
   const mod = (n, m) => {
     return ((n % m) + m) % m;
   };
 
+  setInterval(() => {
+    if (gamepadIndex.value !== null) {
+      const myGamepad = navigator.getGamepads()[gamepadIndex.value];
+      myGamepad.buttons
+        .map((e) => e.pressed)
+        .forEach((isPressed, buttonIndex) => {
+          if (isPressed) {
+            console.log(`Button ${buttonIndex} is pressed`);
+            switch (buttonIndex) {
+              case 14: // left
+                left();
+                break;
+              case 15: //right
+                right();
+                break;
+              case 12: // up
+                up();
+                break;
+              case 13: // down
+                down();
+                break;
+              case 8: // select
+                launchGame(focused.value);
+                break;
+              case 9: // start
+                launchGame(focused.value);
+                break;
+            }
+          }
+        });
+    }
+  }, 200); // 5 times per second
+
   const launchGame = (index) => {
     // console.log(`spusti: ${games[index].gamePath}`);
     exec(games[index].gamePath);
+  };
+
+  const up = () => {
+    focused.value = focused.value - 2;
+    focused.value = mod(focused.value, 4);
+  };
+
+  const down = () => {
+    focused.value = focused.value + 2;
+    focused.value = mod(focused.value, 4);
+  };
+
+  const left = () => {
+    focused.value = focused.value - 1;
+    focused.value = mod(focused.value, 4);
+  };
+
+  const right = () => {
+    focused.value = focused.value + 1;
+    focused.value = mod(focused.value, 4);
   };
 });
 </script>
