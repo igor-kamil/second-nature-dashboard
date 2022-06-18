d32fe7db4c74a77df27cd5bc48030fef75931508
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-06-18 08:11:29 +0200

    add GameItem component + show all games

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.svg" class="mx-auto">
+
+  <div class="grid grid-cols-2 gap-4 p-10">
+    <GameItem name="Budova(nie) Entity" imageSrc="budovanie.png" authors="Petra Kořenková, Kristian Shofranko, Dominik Devečka">
+      Budova(nie) entity je hra, v ktorej sa dozvedáš o spomienkach a histórii budovy, ktorá to v minulosti nemala ľahké. Vytvor si vlastný názor a porozmýšľaj o jej osude.
+    </GameItem>
+
+    <GameItem name="Paper factory" imageSrc="budovanie.png" authors="Leonard Lofaj, Prokop Findeis">
+      Izometrický environment je simuláciou balansu medzi industriálnou výrobou a pôvodnou prírodou. Abstrahovanie rastu a nepomeru do vizuálnej čistej hry. Industrializácia miest ponúka rozvoj, pre pochopenie exponencionálneho rastu je možné simulovať rôzne scenáre aktivity.
+    </GameItem>
+
+    <GameItem name="Luhy" imageSrc="luhy.png" authors="Victoria Ann Bračoková, Ján Konečný, Miroslav Čuridlo">
+      Simulátor chodenia v prostredí Trenčianskych Luhov. V prostredí je možné prežiť problematiku približovania sa k chráneným živočíchom, premenu vzácnych biotopov na sklad odpadu.
+    </GameItem>
+
+    <GameItem name="440" imageSrc="404.png" authors="Adela Lujza Lučeničová, Natália Zajáčiková, Emma Záhradníková">
+      V 3D interaktívnej hre sa môžeme prechádzať prostredím skládky chemického odpadu z bývalých Chemických závodov Juraja Dimitrova v bratislavskej mestskej časti Vrakuňa v období neskoršieho antropocénu, v ktorom už georeliéf nie je tvorený len prírodnými horninami, ale obsahuje stopy a produkty ľudských činností.
+    </GameItem>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GameItem from './components/GameItem.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GameItem
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/GameItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex hover:bg-gray-800 p-5 rounded-2xl">
+    <div class="basis-2/4">
+      <div class="aspect-square">
+        <img :src="require(`@/assets/${imageSrc}`)" alt="" class="w-full h-full">
+      </div>
+    </div>
+    <div class="text-right basis-2/4 mt-auto text-lg">
+      <h2 class="text-white uppercase text-4xl font-bold -ml-18 text-shadow">{{ name }}</h2>
+      <p class="text-white py-5 pl-1">
+        <slot></slot>
+      </p>
+      <div class="text-purple-500 pl-1 uppercase">
+        {{ authors }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameItem',
+  props: {
+    name: String,
+    imageSrc: String,
+    authors: String
+  }
+}
+</script>
