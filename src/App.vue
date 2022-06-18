7d9675739f72324bb40b108899426fe5fbd4273a
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-06-18 10:32:51 +0200

    rewrite to SFC <script setup> + use awds to navigate through games

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,80 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.svg" class="mx-auto">
+  <img alt="Vue logo" src="./assets/logo.svg" class="mx-auto" />
 
   <div class="grid grid-cols-2 gap-4 p-10">
-    <GameItem name="Budova(nie) Entity" imageSrc="budovanie.png" authors="Petra Kořenková, Kristian Shofranko, Dominik Devečka">
-      Budova(nie) entity je hra, v ktorej sa dozvedáš o spomienkach a histórii budovy, ktorá to v minulosti nemala ľahké. Vytvor si vlastný názor a porozmýšľaj o jej osude.
-    </GameItem>
-
-    <GameItem name="Paper factory" imageSrc="budovanie.png" authors="Leonard Lofaj, Prokop Findeis">
-      Izometrický environment je simuláciou balansu medzi industriálnou výrobou a pôvodnou prírodou. Abstrahovanie rastu a nepomeru do vizuálnej čistej hry. Industrializácia miest ponúka rozvoj, pre pochopenie exponencionálneho rastu je možné simulovať rôzne scenáre aktivity.
-    </GameItem>
-
-    <GameItem name="Luhy" imageSrc="luhy.png" authors="Victoria Ann Bračoková, Ján Konečný, Miroslav Čuridlo">
-      Simulátor chodenia v prostredí Trenčianskych Luhov. V prostredí je možné prežiť problematiku približovania sa k chráneným živočíchom, premenu vzácnych biotopov na sklad odpadu.
-    </GameItem>
-
-    <GameItem name="440" imageSrc="404.png" authors="Adela Lujza Lučeničová, Natália Zajáčiková, Emma Záhradníková">
-      V 3D interaktívnej hre sa môžeme prechádzať prostredím skládky chemického odpadu z bývalých Chemických závodov Juraja Dimitrova v bratislavskej mestskej časti Vrakuňa v období neskoršieho antropocénu, v ktorom už georeliéf nie je tvorený len prírodnými horninami, ale obsahuje stopy a produkty ľudských činností.
-    </GameItem>
-</div>
+    <div v-for="game, index in games" :key="game.id">
+      <GameItem
+        :name="game.name"
+        :imageSrc="game.imageSrc"
+        :authors="game.authors"
+        :class="{ 'bg-gray-800 shadow-purple-500 shadow': index === focused }"
+        
+      >
+        {{ game.description }}
+      </GameItem>
+    </div>
+  </div>
 </template>
 
-<script>
-import GameItem from './components/GameItem.vue'
+<script setup>
+import { ref, onMounted } from "vue";
+import GameItem from "./components/GameItem.vue";
 
-export default {
-  name: 'App',
-  components: {
-    GameItem
+const games = [
+  {
+    name: "Budova(nie) Entity",
+    imageSrc: "budovanie.png",
+    description:
+      "Budova(nie) entity je hra, v ktorej sa dozvedáš o spomienkach a histórii budovy, ktorá to v minulosti nemala ľahké. Vytvor si vlastný názor a porozmýšľaj o jej osude.",
+    authors: "Petra Kořenková, Kristian Shofranko, Dominik Devečka",
+  },
+  {
+    name: "Paper factory",
+    imageSrc: "budovanie.png",
+    description:
+      "Izometrický environment je simuláciou balansu medzi industriálnou výrobou a pôvodnou prírodou. Abstrahovanie rastu a nepomeru do vizuálnej čistej hry. Industrializácia miest ponúka rozvoj, pre pochopenie exponencionálneho rastu je možné simulovať rôzne scenáre aktivity.",
+    authors: "Leonard Lofaj, Prokop Findeis",
+  },
+  {
+    name: "Luhy",
+    imageSrc: "luhy.png",
+    description:
+      "Simulátor chodenia v prostredí Trenčianskych Luhov. V prostredí je možné prežiť problematiku približovania sa k chráneným živočíchom, premenu vzácnych biotopov na sklad odpadu.",
+    authors: "Victoria Ann Bračoková, Ján Konečný, Miroslav Čuridlo",
+  },
+  {
+    name: "440",
+    imageSrc: "404.png",
+    description:
+      "V 3D interaktívnej hre sa môžeme prechádzať prostredím skládky chemického odpadu z bývalých Chemických závodov Juraja Dimitrova v bratislavskej mestskej časti Vrakuňa v období neskoršieho antropocénu, v ktorom už georeliéf nie je tvorený len prírodnými horninami, ale obsahuje stopy a produkty ľudských činností.",
+    authors: "Adela Lujza Lučeničová, Natália Zajáčiková, Emma Záhradníková",
+  },
+];
+const focused = ref(0);
+
+onMounted(() => {
+  window.addEventListener("keypress", e => {
+      console.log(e.keyCode);
+      switch (e.keyCode) {
+        case 97: // left
+          focused.value--;
+          break
+        case 100: //right
+          focused.value++;
+          break
+        case 119: // up
+          focused.value = focused.value - 2;
+          break
+        case 115:
+          focused.value = focused.value + 2;
+          break
+      }
+      focused.value = mod(focused.value, 4)
+      console.log("focus: " + focused.value);
+  })
+
+  const mod = (n, m) => {
+    return ((n % m) + m) % m;
   }
-}
+})
 </script>
--- src/components/GameItem.vue
+++ src/components/GameItem.vue
@@ -1,29 +1,34 @@
 <template>
-  <div class="flex hover:bg-gray-800 p-5 rounded-2xl">
+  <div class="flex p-8">
     <div class="basis-2/4">
       <div class="aspect-square">
-        <img :src="require(`@/assets/${imageSrc}`)" alt="" class="w-full h-full">
+        <img
+          :src="require(`@/assets/${props.imageSrc}`)"
+          alt=""
+          class="w-full h-full"
+        />
       </div>
     </div>
     <div class="text-right basis-2/4 mt-auto text-lg">
-      <h2 class="text-white uppercase text-4xl font-bold -ml-18 text-shadow">{{ name }}</h2>
+      <h2 class="text-white uppercase text-4xl font-bold -ml-18 text-shadow">
+        {{ props.name }}
+      </h2>
       <p class="text-white py-5 pl-1">
         <slot></slot>
       </p>
       <div class="text-purple-500 pl-1 uppercase">
-        {{ authors }}
+        {{ props.authors }}
       </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'GameItem',
-  props: {
-    name: String,
-    imageSrc: String,
-    authors: String
-  }
-}
+<script setup>
+import { defineProps } from "vue"
+
+const props = defineProps({
+    'name': String,
+    'imageSrc': String,
+    'authors': String,
+})
 </script>
