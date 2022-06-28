9d54d9d5dcfd9cb35c869c76b009c3b3cc3ddb8f
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-06-29 00:39:08 +0200

    add loader

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,35 @@
   </div>
 
   <div class="mx-auto text-center text-gray-400 pb-2 text-sm">
-    Stlač <span class="bg-gray-700 rounded-xl text-blue-400 px-2 py-1 text-xs font-bold">A</span> pre štart
+    Stlač
+    <span
+      class="bg-gray-700 rounded-xl text-blue-400 px-2 py-1 text-xs font-bold"
+      >A</span
+    >
+    pre štart
+  </div>
+
+  <div
+    class="fixed inset-0 z-50 flex justify-center items-center"
+    v-if="loading"
+  >
+    <div class="bg-black opacity-70 absolute inset-0" />
+    <svg
+      role="status"
+      class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-green-600"
+      viewBox="0 0 100 101"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+        fill="currentColor"
+      />
+      <path
+        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+        fill="currentFill"
+      />
+    </svg>
   </div>
 </template>
 
@@ -40,9 +68,10 @@ const games = [
     imageSrc: "budovanie_entity/preview.png",
     videoSrc: "budovanie_entity/preview.mp4",
     description:
-      "Budova(nie) entity je hra, v ktorej sa dozvedáš o spomienkach a histórii budovy, ktorá to v minulosti nemala ľahké. Vytvor si vlastný názor a porozmýšľaj o jej osude.",
+      "Na každú budovu je nazerané inak. Niektoré zostanú nepovšimnuté celú svoju existenciu a iné rozvíria debaty na niekoľko desaťročí. Príď sa poprechádzať medzi spomienkami na takú, ktorá to počas svojho života nemala ľahké. Konfrontuj sa rôznymi názormi na ňu a zaži cestu budovania entity.",
     authors: "Petra Kořenková, Kristian Shofranko, Dominik Devečka",
-    gamePath: "open /Applications/LittleMousesEncyclopedia.app",
+    gamePath:
+      "/Applications/LittleMousesEncyclopedia.app/Contents/MacOS/LittleMousesEncyclopedia",
   },
   {
     name: "440",
@@ -51,7 +80,8 @@ const games = [
     description:
       "V 3D interaktívnej hre sa môžeme prechádzať prostredím skládky chemického odpadu z bývalých Chemických závodov Juraja Dimitrova v bratislavskej mestskej časti Vrakuňa v období neskoršieho antropocénu, v ktorom už georeliéf nie je tvorený len prírodnými horninami, ale obsahuje stopy a produkty ľudských činností.",
     authors: "Adela Lujza Lučeničová, Natália Zajáčiková, Emma Zahradníková",
-    gamePath: "open /Applications/LittleMousesEncyclopedia.app",
+    gamePath:
+      "/Applications/LittleMousesEncyclopedia.app/Contents/MacOS/LittleMousesEncyclopedia",
   },
   {
     name: "Paper factory",
@@ -59,7 +89,8 @@ const games = [
     description:
       "Izometrický environment je simuláciou balansu medzi industriálnou výrobou a pôvodnou prírodou. Abstrahovanie rastu a nepomeru do vizuálnej čistej hry. Industrializácia miest ponúka rozvoj, pre pochopenie exponencionálneho rastu je možné simulovať rôzne scenáre aktivity.",
     authors: "Leonard Lofaj, Prokop Findeis",
-    gamePath: "open /Applications/LittleMousesEncyclopedia.app",
+    gamePath:
+      "/Applications/LittleMousesEncyclopedia.app/Contents/MacOS/LittleMousesEncyclopedia",
   },
   {
     name: "Luhy",
@@ -67,10 +98,12 @@ const games = [
     description:
       "Simulátor chodenia v prostredí Trenčianskych Luhov. V prostredí je možné prežiť problematiku približovania sa k chráneným živočíchom, premenu vzácnych biotopov na sklad odpadu.",
     authors: "Victoria Ann Bračoková, Ján Konečný, Miroslav Čuridlo",
-    gamePath: "open /Applications/LittleMousesEncyclopedia.app",
+    gamePath:
+      "/Applications/LittleMousesEncyclopedia.app/Contents/MacOS/LittleMousesEncyclopedia",
   },
 ];
 const focused = ref(0);
+const loading = ref(false);
 let gamepadIndex = ref(null);
 
 onMounted(() => {
@@ -82,7 +115,7 @@ onMounted(() => {
     gamepadIndex.value = null;
   });
   window.addEventListener("keypress", (e) => {
-    console.log(e.keyCode);
+    // console.log(e.keyCode);
     switch (e.keyCode) {
       case 97: // left
         left();
@@ -107,13 +140,13 @@ onMounted(() => {
   };
 
   setInterval(() => {
-    if (gamepadIndex.value !== null) {
+    if (gamepadIndex.value !== null && !loading.value) {
       const myGamepad = navigator.getGamepads()[gamepadIndex.value];
       myGamepad.buttons
         .map((e) => e.pressed)
         .forEach((isPressed, buttonIndex) => {
           if (isPressed) {
-            console.log(`Button ${buttonIndex} is pressed`);
+            // console.log(`Button ${buttonIndex} is pressed`);
             switch (buttonIndex) {
               case 14: // left
                 left();
@@ -145,7 +178,11 @@ onMounted(() => {
 
   const launchGame = (index) => {
     // console.log(`spusti: ${games[index].gamePath}`);
-    exec(games[index].gamePath);
+    loading.value = true;
+    let child = exec(games[index].gamePath);
+    child.on("close", function () {
+      loading.value = false;
+    });
   };
 
   const up = () => {
